fb9fd64d18e89078cc29c5a9cda6df0fb51956a3
kervyRivas <a20112128@pucp.pe>
2021-09-29 18:22:31 -0500

    fixing producer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App() {  
+  const [submit, setSubmit] = useState(false);
+  
+  const handleSubmit = (event) => {
+    setSubmit(true);
+  };
+
   return (
     <div className="App">
+      <iframe id="iframe" name="my_iframe" className="frame"></iframe>
       <h1>Form Iris Setosa</h1>
-      <form action="http://nginx/" method="post">
+      <form action="http://localhost:8090/send_data_1" method="post" target="my_iframe" onSubmit={handleSubmit}>
         <label>Sepal length: </label>
         <input name="sepal_length" type="text"></input>
         <br/>
@@ -17,8 +25,9 @@ function App() {
         <label>Petal width: </label>
         <input name="petal_width" type="text"></input>
         <br/>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit"/>
       </form>
+      {submit && <div>Data Send</div>}
     </div>
   );
 }
