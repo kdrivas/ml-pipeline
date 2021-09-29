23a1f68792a5d2f5c727f91c97e417a66d2811ff
kervyRivas <a20112128@pucp.pe>
2021-09-28 21:38:43 -0500

    mod architecture

--- service_1/main.py
+++ service_1/main.py
@@ -1,5 +1,23 @@
 from fastapi import FastAPI, status
+from kafka import KafkaProducer
 
+import os
+import json
+
+KAFKA_SERVER = os.getenv('KAFKA_SERVER')
+KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
+KAFKA_CONN = KAFKA_SERVER + ':' + KAFKA_TOPIC
+KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
+
+def get_kafka_conn():
+  for _ in range(100):
+    try:
+      return KafkaProducer(bootstrap_servers=KAFKA_CONN,
+                          retries=5)
+    except:
+      print('Error: Loading Kafka Producer')
+
+kafka_producer = get_kafka_conn()
 app = FastAPI()
 
 @app.get('/service_1', status_code=status.HTTP_201_CREATED)
@@ -8,4 +26,12 @@ def root():
 
 @app.post('/model_1', status_code=status.HTTP_201_CREATED)
 async def get_predictions_from_model():
+  kafka_producer.send(KAFKA_TOPIC, json.dumps())
+  kafka_producer.flush()
+  return {'Prediction': 'clase'} 
+
+@app.post('/model_1', status_code=status.HTTP_201_CREATED)
+async def get_predictions_from_model():
+  kafka_producer.send(KAFKA_TOPIC, json.dumps())
+  kafka_producer.flush()
   return {'Prediction': 'clase'} 
