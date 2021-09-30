955cb42f4377c523a9e951d1d69bd901d0d678ea
kervyRivas <a20112128@pucp.pe>
2021-09-30 18:01:38 -0500

    adding telegram bot messages

--- consumer/main.py
+++ consumer/main.py
@@ -1,6 +1,17 @@
 import faust
 import os
 import time
+import requests
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+
+def telegram_bot_sendtext(bot_message):
+  send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + CHAT_ID + '&parse_mode=Markdown&text=' + bot_message
+
+  response = requests.get(send_text)
+
+  return response.json()
 
 class data(faust.Record, serializer='json'):
   credit_score_1: float
@@ -24,6 +35,7 @@ app_topic = app.topic('data', value_type=data)
 async def send_notifications(predictions):
   async for pred in predictions:
     print(pred)
+    telegram_bot_sendtext("We found..." + str(pred.credit_score_4))
 
 time.sleep(40)
 app.start()
