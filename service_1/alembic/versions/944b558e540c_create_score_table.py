f19db213f07b0898565214103284147cf3157770
kdrivas <k.rivas@pucp.edu.pe>
2021-10-06 18:20:18 -0500

    creating table

--- service_1/alembic/versions/944b558e540c_create_score_table.py
+++ service_1/alembic/versions/944b558e540c_create_score_table.py
@@ -7,6 +7,7 @@ Create Date: 2021-10-06 18:14:08.246085
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.sql.expression import null
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,12 @@ depends_on = None
 
 
 def upgrade():
-    pass
-
+  op.create_table('predictions', 
+      sa.Column('score_ingreso_1', sa.Integer, index=True, primary_key=True),
+      sa.Column('score_ingreso_2', sa.Float, nullable=False),
+      sa.Column('score_ingreso_3', sa.Float, nullable=False),
+      sa.Column('score_ingreso_4', sa.Float, nullable=False),
+      sa.Column('pred', sa.Integer, nullable=False))
 
 def downgrade():
-    pass
+  op.drop_table('predictions')
