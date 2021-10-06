83c8c25a15d27bf8ba7274c3d2965f2c9a8c978c
kdrivas <k.rivas@pucp.edu.pe>
2021-10-06 14:19:55 -0500

    finish prediction model

---
+++ service_1/app/models.py
@@ -0,0 +1,13 @@
+from enum import Flag
+from sqlalchemy import Column, Integer, Float
+
+from .database import Base
+
+class Prediction(Base):
+  __table_name__ = "predictions"
+
+  id = Column(Integer, primary_key=True, index=True)
+  credit_score_1 = Column(Float)
+  credit_score_2 = Column(Float)
+  credit_score_3 = Column(Float)
+  credit_score_4 = Column(Float)
